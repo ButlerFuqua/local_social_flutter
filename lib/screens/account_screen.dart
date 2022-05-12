d0f688c86e36558adf86b5c64e9c48e76ecf1643
Butler Fuqua <butlerfuqua@Butlers-MBP-2.lan>
2022-05-11 14:41:26 -1000

    Add image upload widget

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase/supabase.dart';
 import 'package:local_social_flutter/components/auth_required_state.dart';
+import 'package:local_social_flutter/components/avatar.dart';
 import 'package:local_social_flutter/utils/constants.dart';
 
 class AccountScreen extends StatefulWidget {
@@ -15,6 +16,8 @@ class AccountScreen extends StatefulWidget {
 class _AccountScreenState extends AuthRequiredState<AccountScreen> {
   final _usernameController = TextEditingController();
   final _websiteController = TextEditingController();
+  String? _userId;
+  String? _avatarUrl;
   var _loading = false;
 
   /// Called once a user id is received within `onAuthenticated()`
@@ -36,6 +39,14 @@ class _AccountScreenState extends AuthRequiredState<AccountScreen> {
     if (data != null) {
       _usernameController.text = (data['username'] ?? '') as String;
       _websiteController.text = (data['website'] ?? '') as String;
+      _avatarUrl = (data['avatar_url'] ?? '') as String;
+
+      if (_avatarUrl == '') {
+        final imageUrlResponse = supabase.storage
+            .from('avatars')
+            .getPublicUrl('default_profile.jpg');
+        _avatarUrl = imageUrlResponse.data!;
+      }
     }
     setState(() {
       _loading = false;
@@ -74,14 +85,37 @@ class _AccountScreenState extends AuthRequiredState<AccountScreen> {
     if (error != null) {
       context.showErrorSnackBar(message: error.message);
     }
+    Navigator.of(context).pushReplacementNamed('/login');
+  }
+
+  /// Called when image has been uploaded to Supabase storage from within Avatar widget
+  Future<void> _onUpload(String imageUrl) async {
+    final response = await supabase.from('profiles').upsert({
+      'id': _userId,
+      'avatar_url': imageUrl,
+    }).execute();
+    final error = response.error;
+    if (error != null) {
+      context.showErrorSnackBar(message: error.message);
+    }
+    setState(() {
+      _avatarUrl = imageUrl;
+    });
+    context.showSnackBar(message: 'Updated your profile image!');
   }
 
   @override
   void onAuthenticated(Session session) {
     final user = session.user;
     if (user != null) {
+      _userId = user.id;
       _getProfile(user.id);
     }
+  }
+
+  @override
+  void onUnauthenticated() {
+    Navigator.of(context).pushReplacementNamed('/login');
   }
 
   @override
@@ -98,6 +132,11 @@ class _AccountScreenState extends AuthRequiredState<AccountScreen> {
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
         children: [
+          Avatar(
+            imageUrl: _avatarUrl,
+            onUpload: _onUpload,
+          ),
+          const SizedBox(height: 18),
           TextFormField(
             controller: _usernameController,
             decoration: const InputDecoration(labelText: 'User Name'),
